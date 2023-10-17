3caed06f9b924ecbe6953f67bcdeaa3c22de87d3
kacpro94 <kacpro944@gmail.com>
2023-10-17 14:46:35 +0200

    cw2

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -8,7 +8,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
 
-            p27();
+            p24();
         }
 
         static void cw1()
@@ -56,9 +56,9 @@ namespace MyApp // Note: actual namespace depends on the project name.
             else if (b > c) { Console.WriteLine("liczba " + b + "jest najwieksza"); }
             else { Console.WriteLine("liczba " + c + " jest najwieksza"); }
         }
-      
-    
-        
+
+
+
         static void cw6()
         {
             Console.WriteLine("wprowadz liczbe zł: ");
@@ -69,13 +69,13 @@ namespace MyApp // Note: actual namespace depends on the project name.
             {
 
                 case 1:
-                    Console.WriteLine("{0:F2} eur ",  zlote / 4.56);
+                    Console.WriteLine("{0:F2} eur ", zlote / 4.56);
                     break;
                 case 2:
-                    Console.WriteLine("{0:F2} usd " , zlote / 4.32);
+                    Console.WriteLine("{0:F2} usd ", zlote / 4.32);
                     break;
                 case 3:
-                    Console.WriteLine("{0:F2} chf " , zlote / 4.77);
+                    Console.WriteLine("{0:F2} chf ", zlote / 4.77);
                     break;
 
             }
@@ -91,7 +91,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
             {
                 Console.Write("Podaj ocenę: ");
                 ocena = Convert.ToInt32(Console.ReadLine());
-                if (ocena>= 3)
+                if (ocena >= 3)
                 {
                     suma += ocena;
                     liczba++;
@@ -106,39 +106,17 @@ namespace MyApp // Note: actual namespace depends on the project name.
             double lokata = 0;
             int lata = 0;
             Console.WriteLine("Podaj kwote na lokacie: ");
-            lokata=Convert.ToDouble(Console.ReadLine());
+            lokata = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Podaj na ile lat jest lokata: ");
-            lata=Convert.ToInt32(Console.ReadLine());
-            for(int i = 1; i <= lata; i++)
+            lata = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= lata; i++)
             {
                 lokata = lokata + lokata * 0.06;
             }
-            Console.WriteLine("lokata po naliczeniu odsetek wynosi: {0:F2}", lokata) ;
+            Console.WriteLine("lokata po naliczeniu odsetek wynosi: {0:F2}", lokata);
         }
         static void p23()
         {
-            double max = Convert.ToDouble(Console.ReadLine());
-            double min = max;
-
-            for ( int i = 2; i <= 5; i++)
-            {
-                double liczba1 = Convert.ToDouble(Console.ReadLine());
-                if (liczba1 > max)
-                {
-                    max = liczba1;
-                }
-                else if(liczba1 < min) { min = liczba1; }
-                
-
-            }
-            Console.WriteLine("Najwieksza liczba to " + max + "a najmniejsza to: " + min);
-        }
-        static void p24()
-        {
-            int k = 120;
-            double S = 1.8;
-            double d = Convert.ToDouble(Console.ReadLine());
-            double pd = 60 + (d - k) * S;
             double max = Convert.ToDouble(Console.ReadLine());
             double min = max;
 
@@ -153,64 +131,99 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
 
             }
-
+            Console.WriteLine("Najwieksza liczba to " + max + "a najmniejsza to: " + min);
         }
-        static void p25()
+        static void p24()
         {
-            Console.WriteLine("podaj liczbe");
-            int liczba=Convert.ToInt32(Console.ReadLine());
-            int wynik = 1;
-            for(int i = 1; i <= liczba; i++)
+            int k = 120;
+            double S = 1.8;
+            Console.WriteLine("Podaj dlugosc skoku: ");
+            double d = Convert.ToDouble(Console.ReadLine());
+            double pd = 60 + (d - k) * S;
+            Console.WriteLine("Podaj 5 ocen: ");
+            int[] punkty = new int[5];
+            for(int j = 0; j <= 4; j++)
             {
-                wynik = wynik * (i);
-            }
-            Console.WriteLine(wynik);
-        }
-        static void p26()
-        {
-           int liczba=Convert.ToInt32( Console.ReadLine());
-            int k = 0;
-           for(int i=2;i<liczba; i++)
-            {
-                if (liczba % i == 0)
+                punkty[j] = Convert.ToInt32(Console.ReadLine());
+                if (punkty[j] > 20 || punkty[j] < 0)
                 {
-                    Console.WriteLine("to nie jest liczba pierwsza");
-                    k = k + 1;
+                    Console.WriteLine("niepoprawna ilosc punktow");
                     break;
                 }
             }
-           if(k == 0)
+            int max = 0;
+            int min = 20;
+            int oceny = 0;
+            for(int i=0;i<=4;i++)
             {
-                Console.WriteLine("to jest l pierwsza");
+                if (punkty[i] > max) { max = punkty[i]; }
+                if (punkty[i] < min) { min = punkty[i]; }
+                oceny = oceny + punkty[i];
             }
-           
+            oceny = oceny - max - min;
+            Console.WriteLine("wynik wynosi: "+ (pd + oceny));
+
         }
-        static void p27()
-        {
-            int a=Convert.ToInt32(Console.ReadLine());
-            int b=Convert.ToInt32(Console.ReadLine());
-            int k = 0;
-            for (int i=a; i<=b; i++)
+
+
+
+
+            static void p25()
             {
-                for (int j = 2; j < i; j++)
+                Console.WriteLine("podaj liczbe");
+                int liczba = Convert.ToInt32(Console.ReadLine());
+                int wynik = 1;
+                for (int i = 1; i <= liczba; i++)
                 {
-                    if (i % j == 0)
+                    wynik = wynik * (i);
+                }
+                Console.WriteLine(wynik);
+            }
+            static void p26()
+            {
+                int liczba = Convert.ToInt32(Console.ReadLine());
+                int k = 0;
+                for (int i = 2; i < liczba; i++)
+                {
+                    if (liczba % i == 0)
                     {
-                        
+                        Console.WriteLine("to nie jest liczba pierwsza");
                         k = k + 1;
-                        continue;
+                        break;
                     }
                 }
                 if (k == 0)
                 {
-                    Console.WriteLine(i+" to jest l. pierwsza");
+                    Console.WriteLine("to jest l pierwsza");
                 }
-                k = 0;
-            }
-        }
-        static void p28()
-        {
 
-        }
+            }
+            static void p27()
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                int k = 0;
+                for (int i = a; i <= b; i++)
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+
+                            k = k + 1;
+                            continue;
+                        }
+                    }
+                    if (k == 0)
+                    {
+                        Console.WriteLine(i + " to jest l. pierwsza");
+                    }
+                    k = 0;
+                }
+            }
+            static void p28()
+            {
+
+            }
     }
 }
